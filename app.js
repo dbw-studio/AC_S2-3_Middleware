bdb64391a0dcaffb1a97a891a0a78afaaf52f8a0
dbw-studio <dbwangstudio@gmail.com>
2021-06-24 15:02:10 +0800

    feat: add middleware

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.use((req, res, next) => {
+  const reqTime = new Date()
+
+  res.on('finish', () => {
+    const resTime = new Date()
+
+    console.log(` ${reqTime.getFullYear()}-${reqTime.getMonth() + 1}-${reqTime.getDate()} ${reqTime.getHours()}:${reqTime.getMinutes()}:${reqTime.getSeconds()} |  ${req.method} from ${req.path} | total time:  ${resTime - reqTime}ms`)
+  })
+  next()
+})
+
+app.get('/', (req, res) => {
+  res.send('列出全部 Todo')
+})
+
+app.get('/new', (req, res) => {
+  res.send('新增 Todo 頁面')
+})
+
+app.get('/:id', (req, res) => {
+  res.send('顯示一筆 Todo')
+})
+
+app.post('/', (req, res) => {
+  res.send('新增一筆  Todo')
+})
+
+app.listen(port, () => {
+  console.log(`App running on port ${port}`)
+})

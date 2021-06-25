dfabd550ef96afb08c15944edc6c4a8c8cba96e9
dbw-studio <dbwangstudio@gmail.com>
2021-06-25 08:47:02 +0800

    refactor: add time log

--- app.js
+++ app.js
@@ -7,7 +7,8 @@ app.use((req, res, next) => {
 
   res.on('finish', () => {
     const resTime = new Date()
-
+    console.log('伺服器收到請求的時間：', reqTime)
+    console.log('伺服器送出回應的時間：', resTime)
     console.log(` ${reqTime.getFullYear()}-${reqTime.getMonth() + 1}-${reqTime.getDate()} ${reqTime.getHours()}:${reqTime.getMinutes()}:${reqTime.getSeconds()} |  ${req.method} from ${req.path} | total time:  ${resTime - reqTime}ms`)
   })
   next()
